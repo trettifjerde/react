7ffb440ffb21c662c0ab0c725184b2d25202e7dd
trettifjerde <sasjandreeva@gmail.com>
2023-02-06 08:49:16 +0100

    userapp remade a bit

--- userapp/src/components/User/Users.js
+++ userapp/src/components/User/Users.js
@@ -1,10 +1,20 @@
-import User from "./User";
+import Card from '../UI/Card';
+import './Users.css';
 
 function Users(props) {
+    let content = <p className='c'>No users yet</p>;
+    if (props.users.length > 0) {
+        content = (<ul className="users">
+            {props.users.map(user => <li key={user.id}>{user.username} ({user.age})</li>)}
+            </ul>
+        );
+    }
     return (
-        <div>
-            {props.users.map(user => <User key={user.id} user={user}/>)}
-        </div>
+        <Card>
+            <h3 className='users-title'>Registered users</h3>
+            {content}
+        </Card>
     )
+
 }
 export default Users;
