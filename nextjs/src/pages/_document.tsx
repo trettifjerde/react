e5de55d8b989cd54211f4594f67111c4fba2a16a
trettifjerde <sasjandreeva@gmail.com>
2023-05-04 17:24:19 +0200

    nextjs: some meta data added

--- nextjs/src/pages/_document.tsx
+++ nextjs/src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>React Meetups</title>
+        <meta name="description" content='Find a React meetup nearby' />
+      </Head>
       <body>
         <Main />
         <NextScript />
